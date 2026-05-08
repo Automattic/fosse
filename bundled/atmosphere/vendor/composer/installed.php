d700dd6513552e1b6706f6c73aae2939fb99d680
Brandon Kraft <public@brandonkraft.com>
2026-05-08 11:15:25 -0500

    chore: sync bundled AP (e9194efa) + Atmo PR 51 (802bef4) (#115)

--- bundled/atmosphere/vendor/composer/installed.php
+++ bundled/atmosphere/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'automattic/atmosphere',
         'pretty_version' => 'dev-trunk',
         'version' => 'dev-trunk',
-        'reference' => '3b50792d2ef794dc5d1f92426462611831398f85',
+        'reference' => '802bef4f88c3943302b33eef5bf5a96622a94e30',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'automattic/atmosphere' => array(
             'pretty_version' => 'dev-trunk',
             'version' => 'dev-trunk',
-            'reference' => '3b50792d2ef794dc5d1f92426462611831398f85',
+            'reference' => '802bef4f88c3943302b33eef5bf5a96622a94e30',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
